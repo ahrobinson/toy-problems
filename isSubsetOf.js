2923384b269539173162832fc2c3eb0171bb597b
Alon Robinson <ahrobins3@gmail.com>
2016-02-08 14:35:32 -0800

    add O(n) solution

--- isSubsetOf.js
+++ isSubsetOf.js
@@ -17,3 +17,32 @@ Array.prototype.isSubsetOf = function() {
   }
   return count === this.length
 };
+
+//O(n)
+
+Array.prototype.isSubsetOf = function() {
+  var arr = arguments[0];
+  var obj = {}
+  var count = 0
+  //this will increase for duplicate values in Array
+  var offset = 0
+  for(var i = 0; i < this.length;i++){
+    if(!obj[this[i]]){
+      obj[this[i]] = 1
+    } else {
+      offset++
+    }
+  }
+
+  for(var i = 0; i <arr.length;i++){
+    if(obj[arr[i]]){
+      obj[arr[i]]++
+    }
+  }
+  for(var key in obj){
+    if(obj[key] > 1){
+      count++
+    }
+  }
+  return count === this.length - offset
+};
