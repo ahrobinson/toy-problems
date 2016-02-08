d8e5964be98c5f9a7c0338d2adb5583857677c86
Alon Robinson <ahrobins3@gmail.com>
2016-02-08 11:17:01 -0800

    add support for negative numbers

--- integerReverse.js
+++ integerReverse.js
@@ -4,9 +4,21 @@ DO NOT CONVERT n INTO A STRING OR ARRAY.
 Only use integers and math in your solution.
 */
 
-function reverseInteger (n) {
+function reverse (n) {
   // Write your code here, and
   // return your final answer.
+  if(n < 0){
+    n = Math.abs(n);
+
+    var revNum = 0;
+    while(n !== 0){
+      revNum *= 10;
+      revNum += n % 10;
+      n = Math.floor(n/10)
+    }
+    return -revNum
+  }
+
   var revNum = 0;
   while(n !== 0){
     revNum *= 10;
